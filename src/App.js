7d7c04e635ff9e60cebe8ee6f9e13f29101026a3
IgorBaio <igorbaiosoares@gmail.com>
2021-09-02 16:53:02 -0300

    First version

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import AddProduto from './Components/AddProduto';
+import Table from './Components/Table';
+import ViewProduto from './Components/ViewProduto';
+import Login from './screens/Login';
+import Chat from './screens/Chat';
+
+export default () => {
+      return (
+        <Router>
+          <Route exact path="/" component={Table} />
+          <Route exact path="/log" component={Login} />
+          <Route exact path="/mess" component={Chat} />
+          <Route exact path="/add" component={AddProduto} />
+          <Route exact path="/view" component={Table} />
+          <Route exact path="/produto_view" component={ViewProduto} />
+        </Router>  
+      );
+}
+
+
